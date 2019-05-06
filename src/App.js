08af1846a2991ef038266ec1f350478f7ca1faf4
Drake Young <36168851+drake-young@users.noreply.github.com>
2019-05-06 12:36:27 -0600

    CSS should work now

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductPurchase from './Components/ProductPurchase';
 import {HashRouter,Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Stores/store';
+import './App.css';
 
 class App extends Component 
 {
