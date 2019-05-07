8e46cc1363a061b487955de6e02d61d52ad1e340
Drake Young <36168851+drake-young@users.noreply.github.com>
2019-05-07 11:06:46 -0600

    added product info to purchase page

--- src/Components/ProductPurchase.js
+++ src/Components/ProductPurchase.js
@@ -2,8 +2,9 @@ import React, { Component} from 'react';
 import { connect } from 'react-redux';
 import Purchase from './Purchase';
 import { resetPurchase } from '../Actions/purchActions';
-import { Button } from 'react-bootstrap';
+import { Button, Image, Col, Row } from 'react-bootstrap';
 import MapContainer from './Map';
+import gauntlet from '../infg.png';
 
 class ProductPurchase extends Component {
 
@@ -39,7 +40,29 @@ class ProductPurchase extends Component {
 
         const formNotSubmitted = (
             <div>
-                { this.state.toggleReceipt ? <div></div> : <Purchase />}
+				<Row>
+					<Col sm={1} md={1}>
+						&nbsp;
+					</Col>
+					<Col sm={11} md={11}>
+						<h1 className="prod-header">Buy the Infinity Gauntlet!</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm={7} md={6} className="prod-img text-right">
+						<Image width="288" height="450" className="image" src={gauntlet} alt="Infinity Gauntlet" />
+					</Col>
+					<Col xs={1} sm={0} md={0} lg={0} xl={0}>&nbsp;</Col>
+					<Col xs={10} sm={3} md={3} lg={2} className="prod-info"><br/>
+						<h4>Cost: Everything</h4><br/>
+						<h4>Color: Gold</h4><br/>
+						<h4>Product Description:</h4>
+						<p>The Infinity Gauntlet was designed to hold six of the &quot;soul gems&quot;, better known as the Infinity Gems. When used in combination their already impressive powers amke the wearer able to do anything they want. It was gathered by the mad Titan Thanos as he took the gems from the Elders of the Universe that had originally carried them.</p>
+						<h5>** Infinity Gems Not Included **</h5>
+					</Col>
+					<Col xs={1} sm={0} md={0} lg={0} xl={0}>&nbsp;</Col>
+				</Row>
+                <Purchase />
             </div>
         );
         const formSubmitted = (
